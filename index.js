880c0bcdf950e0ad5013d8095c9c06284f4275f8
PGJ <pol.gozalvez@mataro.epiaedu.cat>
2024-10-10 20:28:21 +0200

    fix: add new exercices v2

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // EXERCISE 1: Selecting Elements
-// Select the element with the ID 'header' and log it to the console.
+// TODO: Select the element with the ID 'mainTitle' and log it to the console.
+let elementTitle = document.getElementById("mainTitle");
+console.log(elementTitle);
+
+// Exercise 2: Changing HTML Content
+// TODO: Select all paragraph elements and change the content of the second paragraph to "This paragraph has been changed."
+let elementContent = document.getElementsById("content");
+elementContent.innerHTML = "<p>Updated Content</p>";
+
+// EXERCISE 3: Changing CSS Styles
+// Change the background color of the element with the ID 'content' to 'lightgreen'.
+let elementBackground = document.getElementById("content");
+elementBackground.style.backgroundColor = "lightgreen";
